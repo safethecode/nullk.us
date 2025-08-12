9b4d64cc3e764ee00a34a251f3ba75869ec44349
safethecode <iam@sonjim.in>
2025-08-12 20:00:15 +0900

    feat: 문제집 페이지 추가

--- apps/service/middleware.ts
+++ apps/service/middleware.ts
@@ -1,25 +1,24 @@
-import { NextRequest, NextResponse } from "next/server"
+import { type NextRequest, NextResponse } from 'next/server';
 
 const subDomainMapper = {
-  "stage-engr": "/stage-engr"
-} as const
+  'stage-engr': '/stage-engr',
+} as const;
 
 export const middleware = (request: NextRequest) => {
-  const host = request.headers.get("host") || ""
-  const subdomain = host.split(".")[0] || ""
+  const host = request.headers.get('host') || '';
+  const subdomain = host.split('.')[0] || '';
 
   if (subdomain in subDomainMapper) {
-    const newUrl = request.nextUrl.clone()
-    newUrl.pathname = subDomainMapper[subdomain as keyof typeof subDomainMapper]
+    const newUrl = request.nextUrl.clone();
+    newUrl.pathname =
+      subDomainMapper[subdomain as keyof typeof subDomainMapper];
 
-    return NextResponse.rewrite(newUrl)
+    return NextResponse.rewrite(newUrl);
   }
 
-  return NextResponse.next()
-}
+  return NextResponse.next();
+};
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|/_next/image|favicon\\.ico|.*\\.png).*)",
-  ],
-}
+  matcher: ['/((?!api|_next/static|/_next/image|favicon\\.ico|.*\\.png).*)'],
+};

1fa8ea03b8c33891684bf48e2a05d5516b2c3fc3
Jimin (Sam) Son <iam@Sonjim.in>
2025-08-12 20:12:25 +0900

    fix(middleware): 미들웨어에 이미지 규칙 수정 (#2)

--- apps/service/middleware.ts
+++ apps/service/middleware.ts
@@ -20,5 +20,7 @@ export const middleware = (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: ['/((?!api|_next/static|/_next/image|favicon\\.ico|.*\\.png).*)'],
+  matcher: [
+    '/((?!api|_next/static|/_next/image|favicon\\.ico|.*\\.png|.*\\.svg).*)',
+  ],
 };

1d031915909edcac716c16a5ae6ad3a7bad142ec
safethecode <sam@nullk.us>
2026-06-20 17:38:55 +0900

    chore: 블로그 내비게이션과 라우트를 제거

--- apps/service/lib/navigation/constants.ts
+++ apps/service/lib/navigation/constants.ts
@@ -1,7 +1,6 @@
 import {
   FolderOpen,
   type LucideIcon,
-  PenTool,
   Trophy,
   User,
 } from "lucide-react";
@@ -22,11 +21,6 @@ export const landingNavigationItems: NavigationItem[] = [
     label: "참여 프로젝트",
     href: "/projects",
     icon: FolderOpen,
-  },
-  {
-    label: "블로그",
-    href: "/blogs",
-    icon: PenTool,
   },
   {
     label: "도전과제",
